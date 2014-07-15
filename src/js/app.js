c7fca8fd67b536398c768220b813295edd679601
GimYoungPhil <phil@GimYoungPhilui-MacBook-Air.local>
2014-07-15 18:30:57 +0900

    fb simpleLogin

--- src/js/app.js
+++ src/js/app.js
@@ -30,7 +30,6 @@ angular.module('app').controller('MainCtrl', ['$scope', '$http', '$firebase', fu
 
   function init() {
     loadCarouselData();
-    loadMoviewData();
   }
 
   function loadCarouselData() {
@@ -43,10 +42,10 @@ angular.module('app').controller('MainCtrl', ['$scope', '$http', '$firebase', fu
   }
 
   function loadMoviewData() {
-    var movieRef = new Firebase("https://imdb-top-250.firebaseio.com/movieList");
+    // var movieRef = new Firebase("https://imdb-top-250.firebaseio.com/movieList");
     // Automatically syncs everywhere in realtime
     //$scope.moviewList = $firebase(movieRef);
-    console.log($firebase(movieRef));
+    // console.log($firebase(movieRef));
   }
 
 }]);
@@ -55,7 +54,6 @@ angular.module('app').controller('ModalInstanceCtrl', ['$scope', function($scope
   $scope.ok = function () {
     $modalInstance.close($scope.selected.item);
   };
-
 }]);
 
 angular.module('app').controller('MovieCtrl', ['$scope','$modal','$http','$log', 'ModalInstanceCtrl', function($scope, $modal, $http, $log, ModalInstanceCtrl) {
@@ -89,6 +87,36 @@ angular.module('app').controller('MovieCtrl', ['$scope','$modal','$http','$log',
 
 
 }]);
+
+angular.module('app').controller('HeaderCtrl', ['$scope', '$firebase', '$firebaseSimpleLogin', function ($scope, $firebase, $firebaseSimpleLogin) {
+  var ref = new Firebase("https://gim-bluelay.firebaseio.com/");
+  $scope.auth = $firebaseSimpleLogin(ref);
+}]);
+
+/*
+{
+  "id":"113636980213637111257",
+  "uid":"google:113636980213637111257",
+  "displayName":"YoungPhil Gim",
+  "provider":"google",
+  "thirdPartyUserData":
+  {
+    "id":"113636980213637111257",
+    "email":"youngphil.gim@gmail.com",
+    "verified_email":true,
+    "name":"YoungPhil Gim",
+    "given_name":"YoungPhil",
+    "family_name":"Gim",
+    "link":"https://plus.google.com/113636980213637111257",
+    "picture":"https://lh3.googleusercontent.com/-caKk0SdNW-Y/AAAAAAAAAAI/AAAAAAAAADQ/LtOG4ZHCA4o/photo.jpg",
+    "gender":"male",
+    "locale":"ko"
+  },
+  "accessToken":"ya29.QwCsB6ypge-E0hsAAAD9JXOqoChSZZC2gmiSzm9d7D0FpWrjDnaLIf-tdPr2AA",
+  "email":"youngphil.gim@gmail.com",
+  "firebaseAuthToken":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjE0MDU0ODY3NjgsInYiOjAsImQiOnsiaWQiOiIxMTM2MzY5ODAyMTM2MzcxMTEyNTciLCJ1aWQiOiJnb29nbGU6MTEzNjM2OTgwMjEzNjM3MTExMjU3IiwicHJvdmlkZXIiOiJnb29nbGUiLCJlbWFpbCI6InlvdW5ncGhpbC5naW1AZ21haWwuY29tIn0sImlhdCI6MTQwNTQwMDM2N30.zO21U3PGt5UpdNonj109sqpZ_ovyTwI_30mRz62KMKI"
+}
+*/
 
 
 // angular.module('app').controller('AppCtrl', ['$scope', 'i18nNotifications', 'localizedMessages', function($scope, i18nNotifications) {
